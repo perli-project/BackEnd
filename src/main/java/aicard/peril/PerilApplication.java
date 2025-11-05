91cc1887c50afcfd5b31ddd77763bb0d3a213558
LEE HAEIN <saranghein@gmail.com>
2025-11-05 15:34:58 +0900

    :broom: Chore: 의존성 업데이트 - 의존성 추가했습니다.

---
+++ src/main/java/aicard/peril/PerilApplication.java
@@ -0,0 +1,13 @@
+package aicard.peril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aicard/peril/PerilApplicationTests.java
@@ -0,0 +1,13 @@
+package aicard.peril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
